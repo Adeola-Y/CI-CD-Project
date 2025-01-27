02c6da43086b46a840d54adce7404ec1cedec591
Adeola-Y <113193008+Adeola-Y@users.noreply.github.com>
2025-01-26 20:05:39 -0600

    Update vitest.config.js configured for C8

--- project_template/project/vitest.config.js
+++ project_template/project/vitest.config.js
@@ -2,7 +2,14 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    environment: 'jsdom',
-    globals: true
-  }
+    environment: 'jsdom',    // keep jsdom environment for testing
+    globals: true,           // enables global functions 
+    
+    // code coverage
+    coverage: {
+      provider: 'c8',        // c8 is a tool for coverage 
+      reporter: ['text', 'html'], // outputs coverage in terminal
+      reportsDirectory: './coverage', // where the coverage reports arestored
+    },
+  },
 });
