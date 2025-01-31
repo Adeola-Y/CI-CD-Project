a22f67ff5e7418965629a9737bb241737b27313a
Adeola-Y <113193008+Adeola-Y@users.noreply.github.com>
2025-01-30 18:38:35 -0600

    Update vitest.config.js

--- project_template/project/vitest.config.js
+++ project_template/project/vitest.config.js
@@ -8,5 +8,7 @@ export default defineConfig({
       reporter: ['text', 'json', 'html', 'lcov'], // generates text, json, html, and lcov reports
       all: true, // coverage for all files 
     },
+    globals: true, // Use global variables (this will make `beforeAll` work)
+    setupFiles: ['./setupTests.js'], // Point to your setup file
   },
 });
