8c4a25954a2abbc475d441e1c4cc04138a3fe9d5
Adeola-Y <113193008+Adeola-Y@users.noreply.github.com>
2025-01-30 18:09:58 -0600

    Update vitest.config.js

--- project_template/project/vitest.config.js
+++ project_template/project/vitest.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     globals: true,
     coverage: {
       provider: 'v8',
-      reporter: ['text', 'html'],
+      reporter: ['text', 'lcov', 'json-summary'],
       reportsDirectory: './coverage',
     },
   },
